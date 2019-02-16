92611ddf8474fc51b39289ed9ea80b5fab328963
Rene Cocom <fasnimo@gmail.com>
2019-02-16 18:06:37 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log("HELLO");
+}
+
+function logWhisper(string) {
+  console.log("hello");
+}
+
+function sayHiToGrandma(string) {
+ 
+  if (string.toLowerCase === "hello") {
+  console.log("I can't hear you.");
+ 
+} else if (string.toUpperCase === "HELLO") {
+  return ("YES INDEED!");
+ 
+} else if (string("I love you, Grandma!") === "playful") {
+  console.log("Playing tug-of-war.")
+ 
+} else if (dog === "cuddly") {
+  console.log("Snuggling.")
+ 
+} else { 
+  console.log("Reading newspaper.")
+}
+}
